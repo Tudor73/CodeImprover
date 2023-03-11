8a38130df9ef575a7c7697808f0df228abe7212c
Flavia Podariu <flaviapodariu@gmail.com>
2023-03-11 15:53:14 +0200

    Put code suggestions(string) on screen (not parsed yet)

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,24 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Configuration, OpenAIApi } from "openai";
+import { url } from 'inspector';
+import { BaseAPI } from 'openai/dist/base';
+
+
+async function aiReq(questionForGPT: string) {
+    let configuration = new Configuration({
+        apiKey: "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"        
+    })
+    let openai = new OpenAIApi(configuration);
+    return openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ "role": "user", "content": questionForGPT }],
+        temperature: 0.2
+    })
+
+}
+
 
 
 // This method is called when your extension is activated
@@ -21,16 +39,25 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		
 		const editor = vscode.window.activeTextEditor;
-		console.log(editor?.document.languageId)
+		const language = editor?.document.languageId
 		const selection = editor?.selection;
+        let text = ""
 		if (selection) {
-			const text = editor.document.getText(selection);
+			text = editor.document.getText(selection);
 			console.log(text);
 		}
-		vscode.window.showInformationMessage('Hello World from CodeImprover!');
-	});
+        vscode.window.showInformationMessage('Hello World from CodeImprover!');
+
+        let improvements = `Write only code. Better ways to write this in ${language}: ${text}`
+        aiReq(improvements).then((res) => {
+            const response = res.data.choices[0].message?.content as string
+            vscode.window.showInformationMessage(response);
+
+        }).catch((err) => console.log(err))
+        
 
 	context.subscriptions.push(disposable);
+});
 }
 
 // This method is called when your extension is deactivated
