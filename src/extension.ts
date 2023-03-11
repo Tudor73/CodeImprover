2d3d33034ed0e85532d738313f49ed06ed2b3732
Tudor73 <tudorminciunescu@gmail.com>
2023-03-11 12:12:05 +0200

    get highlighted text

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -13,9 +14,19 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+
+	
 	let disposable = vscode.commands.registerCommand('codeimprover.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
+		
+		const editor = vscode.window.activeTextEditor;
+		console.log(editor?.document.languageId)
+		const selection = editor?.selection;
+		if (selection) {
+			const text = editor.document.getText(selection);
+			console.log(text);
+		}
 		vscode.window.showInformationMessage('Hello World from CodeImprover!');
 	});
 
